424d77f6bb36143dfc30d7cceb686c7ebe4b44c0
marcus fernando <marcusfcbarbosa@gmail.com>
2019-01-20 23:13:20 -0200

    Criado a primeira parte do webpack para suportar uma aplicação react

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack') //webpack executa em cima do node.js
+
+//expoe um objeto que representa toda a configuração
+module.exports ={
+    entry: './ex/index.js',
+    output:{
+        path: __dirname+ '/public',//onde sera gerada a saida do javascript (__dirname = diretorio local variavel de ambiente)
+        filename: './bundle.js',//lembrando que pode ser qualquer nome
+
+    },
+    devServer:{
+        port:8080,
+        contentBase: './public',
+    },
+    module:{
+        loaders:[{
+            test: /.jsx?$/, //observe a extensao dos  arquivos react .jsx
+            loader:'babel-loader',//para poder usar o import
+            exclude:/node_modules/,
+            query:{
+                presets:['es2015','react']
+            }
+
+        }]
+    }
+}
