2e9684c3318a715db3f4f38758757eacfe075f49
marcus fernando <marcusfcbarbosa@gmail.com>
2019-01-22 22:35:44 -0200

    Primeira execução do React

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack') //webpack executa em cima do node.js
 
 //expoe um objeto que representa toda a configuração
 module.exports ={
-    entry: './ex/index.js',
+    entry: './ex/index.jsx',//a entrada é uma arquivo de extensao .jsx padrão do react
     output:{
         path: __dirname+ '/public',//onde sera gerada a saida do javascript (__dirname = diretorio local variavel de ambiente)
         filename: './bundle.js',//lembrando que pode ser qualquer nome
