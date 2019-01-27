8effc5f80c6ace8a266a150d70aef63bd3f4f543
marcus fernando <marcusfcbarbosa@gmail.com>
2019-01-27 00:15:51 -0200

    Componente por classe, formato padrao

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ClassComponente from './classComponente'
 
 
 //passando a propriedade LastName para as filhas dentro de Family
 ReactDOM.render(
-    
+    <ClassComponente value='Componente Classe' />
 
 , document.getElementById('app'))
