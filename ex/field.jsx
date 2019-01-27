ffbd9a04ef7e593d9dbb6cab2630956d19882ab1
marcus fernando <marcusfcbarbosa@gmail.com>
2019-01-27 12:18:59 -0200

    Componente controlado, deixando mais facil de entender

--- ex/field.jsx
+++ ex/field.jsx
@@ -5,21 +5,22 @@ class Field extends Component {
 
     constructor(props){
         super(props)        
-        this.state ={ value: props.initialValue }
+        this.state ={ valorEstado: props.initialValue }
         this.handleChange = this.handleChange.bind(this)
     }
 
     //sempre que digitar alguma coisa no campo, esse evento estara responavewl por ele
     //target do event seria o input
+    //<input onChange={this.handleChange} value={this.state.value || ''} /> dessa forma ele nunca ira setar um componente sem controle
     handleChange(event){
-        this.setState({ value : event.target.value })
+        this.setState({ valorEstado : event.target.value })
     }
 
     render(){
         return(
             <div>
-                <label> { this.state.value }</label> <br/>
-                <input onChange={this.handleChange} value={this.state.value} />
+                <label> { this.state.valorEstado }</label> <br/>
+                <input onChange={this.handleChange} value={this.state.valorEstado || ''} />
             </div>
         )
     }
