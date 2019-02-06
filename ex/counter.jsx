9307061a72516d72ad0285903da90c488d3c9ba7
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-02-06 15:47:07 -0200

    Integração react Rexu parte 2, criando um contador

--- ex/counter.jsx
+++ ex/counter.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+//Componente por função
+import  { bindActionCreators } from 'redux'//liga as ações criadas com os Dispatchers
+import { connect } from 'react-redux'//usado para conectar react com redux
+import { dec,inc,stepChanged } from  './counterActions'
+
 
 const Counter =(props) =>(
-    <div>Ok</div>
+    <div>
+        <h1> {props.counter.number} </h1>
+
+        <input onChange={props.stepChanged}
+            value={ props.counter.steps }  type='number'/>
+            
+        <button onClick={ props.dec }> DEC </button>
+        <button onClick={ props.inc }> INC </button>
+    </div>
 )
 
-export default Counter
+const mapStateToProps = state =>({ counter: state.counter })
+
+//Liga as minhas ações com o dispatch do redux
+const mapDispatchToProps = 
+dispatch => bindActionCreators({ 
+            dec,
+            inc,
+            stepChanged 
+    },dispatch)
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counter)
--- ex/index.jsx
+++ ex/index.jsx
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import Field from './field'
-import fieldReducer from './fieldReducer'
+import counterReducer from './counterReducer'
+import Counter from './counter'
 
 
 //Esse combineReducers que ta controlando seu estado
 const reducers = combineReducers({
-    field: fieldReducer
+    counter: counterReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <Counter />
     </Provider>
     
 , document.getElementById('app'))
